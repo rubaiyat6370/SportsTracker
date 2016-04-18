967c5e2d4073bc521d8b06d09c9bf9aa01e82b68
syeda <syeda@localhost>
2016-04-19 02:23:45 +0600

    Layout Updated with new View

--- SportsTracker/Models/DbModel/User.cs
+++ SportsTracker/Models/DbModel/User.cs
@@ -11,9 +11,9 @@ namespace SportsTracker.Models.DbModel
     {
         public int Id { get; set; }
         public int Age { get; set; }
-        public double Weight { get; set; }
+        public decimal Weight { get; set; }
         public string Email { get; set; }
-        public DateTime Birthdate { get; set; }
+        public DateTime? Birthdate { get; set; }
         public int UserProfileId { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
