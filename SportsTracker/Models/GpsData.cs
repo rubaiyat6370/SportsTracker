e5e053857e23de7e9281e29e271aa56da0e1959c
syeda <syeda@localhost>
2016-04-26 04:08:10 +0600

    refactoring

---
+++ SportsTracker/Models/GpsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsTracker.Models
+{
+    public class GpsData
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Elevation { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ SportsTracker/Models/ViewModel/LocationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SportsTracker.Models.ViewModel
+{
+    public class LocationViewModel
+    {
+        public List<GpsData> GpsDatas { get; set; }
+        public double Distance { get; set; }
+        public double Speed { get; set; }
+    }
+}
