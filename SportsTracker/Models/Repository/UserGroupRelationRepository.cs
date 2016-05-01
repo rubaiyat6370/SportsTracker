14007b9f10efe0eaeb1e45f99f958f511fd707d3
mumu <mumu@localhost>
2016-05-01 15:04:38 +0600

    Groupmember removed

--- SportsTracker/Models/Repository/UserGroupRelationRepository.cs
+++ SportsTracker/Models/Repository/UserGroupRelationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using SportsTracker.Models.DbModel;
@@ -8,24 +9,24 @@ namespace SportsTracker.Models.Repository
 {
     public class UserGroupRelationRepository
     {
-        SportsTrackerContext db = null;
+        SportsTrackerContext _db = null;
 
         public UserGroupRelationRepository()
         {
-            db = new SportsTrackerContext();
+            _db = new SportsTrackerContext();
         }
 
         public bool Add(UserGroupRelation groupMember)
         {
-            db.UserGroupRelations.Add(groupMember);
-            return db.SaveChanges() > 0;
+            _db.UserGroupRelations.Add(groupMember);
+            return _db.SaveChanges() > 0;
         }
 
         public ISet<User> MemberListByGroupId(int id)
         {
            // string query = "SELECT * FROM User,UserGroupRelation WHERE User.UserProfileId=UserGroupRelation.UserId";
-            var user = from usr in db.Users
-                join member in db.UserGroupRelations on usr.UserProfileId equals member.UserId
+            var user = from usr in _db.Users
+                join member in _db.UserGroupRelations on usr.UserProfileId equals member.UserId
                 where (member.Groupid == id)
                 select usr;
             user.ToList();
@@ -34,11 +35,23 @@ namespace SportsTracker.Models.Repository
             return u ;
         }
 
-        public List<Post> groupPostList(int id)
+        public List<Post> GroupPostList(int id)
         {
-            return db.Posts.Where(p => p.GroupId == id).ToList();
+            return _db.Posts.Where(p => p.GroupId == id).ToList();
 
-        } 
+        }
+
+        public bool DeleteMember(int groupId, int userId)
+        {
+            var u = from ur in _db.UserGroupRelations
+                where (ur.Groupid == groupId && ur.UserId == userId)
+                select ur;
+            foreach (var userGroupRelation in u)
+            {
+                _db.UserGroupRelations.Remove(userGroupRelation);
+            }
+            return _db.SaveChanges() > 0;
+        }
 
         
     }
