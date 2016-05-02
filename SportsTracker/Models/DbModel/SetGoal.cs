8132be11028d208a83bf65c58722914914a644c8
syeda <syeda@localhost>
2016-05-03 03:18:03 +0600

    set goal

---
+++ SportsTracker/Models/DbModel/SetGoal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SportsTracker.Models.DbModel
+{
+    [Table("SetGoal")]
+    public class SetGoal
+    {
+        public int Id { get; set; }
+        public int UserProfileId { get; set; }
+        public double Distance { get; set; }
+        public double Calorie { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime EndOn { get; set; }
+        public bool IsAchieved { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
